b6284c259fef0503ee9466a92bc863bd9541617e
julia-richards <julia.m.richards@gmail.com>
2020-12-29 15:06:07 -0800

    adds likes migration and fixes errors

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -7,7 +7,7 @@ user_routes = Blueprint('users', __name__)
 
 
 @user_routes.route('/')
-# @login_required
+@login_required
 def users():
     users = User.query.all()
     return {"users": [user.to_dict() for user in users]}
@@ -17,7 +17,7 @@ def users():
 
 
 @user_routes.route('/<int:id>')
-# @login_required
+@login_required
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
@@ -26,19 +26,19 @@ def user(id):
 # Password change
 # Update the routes for specific user
 @user_routes.route('/<int:id>', methods=['PUT'])
-# @login_required
+@login_required
 def update_user(id):
     form = EditForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     user = User.query.get(id)
     data = request.json
-    if form.data['username']: 
+    if form.data['username']:
         user.username = data['username']
-    if form.data['email']: 
+    if form.data['email']:
         user.email = data['email']
-    if form.data['about']: 
+    if form.data['about']:
         user.about = data['about']
-    if form.data['image_url']: 
+    if form.data['image_url']:
         user.image_url = data['image_url']
     db.session.commit()
     return user.to_dict()
