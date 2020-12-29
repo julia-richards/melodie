935ca610b26f9399fecc9445e17b866444d7314a
salina kuo <salina.kuo97@gmail.com>
2020-12-29 12:04:04 -0500

    song post route

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
 from app.models import Song, User
+from auth_routes import validation_errors_to_error_messages
 
 song_routes = Blueprint('songs', __name__)
 
@@ -14,10 +15,16 @@ def songs():
 def update_song(id):
     song = Song.query.get(id)
     if song.user_id == current_user.id:
-        song = Song(
-            #get data from form
-        )
-        db.session.add(song)
-        db.session.commit()
-        return 'Success' #TODO:change me
-    return {'errors': 'User does not match creator'}
+        if form.validate_on_submit():
+            song = Song(
+                title=form.data['title'],
+                length=form.data['length'],
+                description=form.data['description'],
+                image_url=form.data['image_url'],
+                song_url=form.data['song_url']
+            )
+            db.session.add(song)
+            db.session.commit()
+            return song.to_dict()
+        return {'errors': validation_errors_to_error_messages(form.errors)}
+    return {'errors': 'Only the artist can delete this song'}
