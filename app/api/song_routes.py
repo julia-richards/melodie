c3794fd33b913b3ed9f15eb14d932344335aeb68
julia-richards <julia.m.richards@gmail.com>
2020-12-29 13:06:25 -0800

    works on song list feature

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,10 +1,21 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
 from app.models import Song, User
-from auth_routes import validation_errors_to_error_messages
-from forms.song_form import SongForm
+# from auth_routes import validation_errors_to_error_messages
+from app.forms.song_form import SongForm
 
 song_routes = Blueprint('songs', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
 
 
 @song_routes.route('/')
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,55 +4,68 @@ import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Songs from "./components/Songs";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./services/auth";
 import SongForm from "./components/SongForm";
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loaded, setLoaded] = useState(false);
+	const [authenticated, setAuthenticated] = useState(false);
+	const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    (async() => {
-      const user = await authenticate();
-      if (!user.errors) {
-        setAuthenticated(true);
-      }
-      setLoaded(true);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const user = await authenticate();
+			if (!user.errors) {
+				setAuthenticated(true);
+			}
+			setLoaded(true);
+		})();
+	}, []);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Route path="/login" exact={true}>
-        <LoginForm
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/songs/upload" >
-        <SongForm />
-      </Route>
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      </Route>
-      <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-        <UsersList/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-        <User />
-      </ProtectedRoute>
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>My Home Page</h1>
-      </ProtectedRoute>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<NavBar setAuthenticated={setAuthenticated} />
+			<Route path="/login" exact={true}>
+				<LoginForm
+					authenticated={authenticated}
+					setAuthenticated={setAuthenticated}
+				/>
+			</Route>
+			<Route path="/songs/upload">
+				<SongForm />
+			</Route>
+			<Route path="/sign-up" exact={true}>
+				<SignUpForm
+					authenticated={authenticated}
+					setAuthenticated={setAuthenticated}
+				/>
+			</Route>
+			<ProtectedRoute
+				path="/users"
+				exact={true}
+				authenticated={authenticated}
+			>
+				<UsersList />
+			</ProtectedRoute>
+			<ProtectedRoute
+				path="/users/:userId"
+				exact={true}
+				authenticated={authenticated}
+			>
+				<User />
+			</ProtectedRoute>
+			<ProtectedRoute path="/" exact={true} authenticated={authenticated}>
+				<h1>My Home Page</h1>
+				<Songs />
+			</ProtectedRoute>
+		</BrowserRouter>
+	);
 }
 
 export default App;
