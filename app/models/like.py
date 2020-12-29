9a73e3072335fd6d7cb7427c16aeebf1d9bc48c7
Rashmi <monajain99@hotmail.com>
2020-12-29 16:35:12 -0500

    likesBranch

--- app/models/like.py
+++ app/models/like.py
@@ -3,15 +3,22 @@ from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Like(db.Model):
-    __tablename__ = "likes"
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False,)
-    songId = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False,)
+likes = db.Table(
+    "likes",
+    db.Model.metadata,
+    db.Column("userId", db.Integer, db.ForeignKey('users.id'), primary_key=True )
+    db.Column("songId", db.Integer, db.ForeignKey('songs.id'), primary_key=True )
+)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "userId": self.userId,
-            "songId": self.songId,
-        }
+# class Like(db.Model):
+#     __tablename__ = "likes"
+#     id = db.Column(db.Integer, primary_key=True)
+#     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False,)
+#     songId = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False,)
+
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "userId": self.userId,
+#             "songId": self.songId,
+#         }
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,15 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
+db = SQLAlchemy()
+
+likes = db.Table(
+    "likes",
+    db.Model.metadata,
+    db.Column("userId", db.Integer, db.ForeignKey("users.id")),
+    db.Column("songId", db.Integer, db.ForeignKey("songs.id"))
+)
 
 class User(db.Model, UserMixin):
   __tablename__ = 'users'
@@ -10,7 +19,9 @@ class User(db.Model, UserMixin):
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
   about = db.Column(db.String(2000), nullable = True)
-  image_url = db.Column(db.String(300), nullable= True)
+  image_url = db.Column(db.String(300), nullable=True)
+  
+  songs = db.relationship("Song", secondary=likes, back_populates="users")
 
   @property
   def password(self):
@@ -34,3 +45,28 @@ class User(db.Model, UserMixin):
       "about": self.about,
       "image_url": self.image_url
     }
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(225), nullable = False)
+    length = db.Column(db.Float, nullable = False)
+    description = db.Column(db.String(2000))
+    image_url = db.Column(db.String)
+    song_url = db.Column(db.String)
+    user_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+    
+    users = db.relationship("User", secondary=likes, back_populates="likes")
+
+    def to_dict(self):
+      return {
+        "id": self.id,
+        "title": self.title,
+        "length": self.length,
+        "description": self.description,
+        "image_url": self.image_url,
+        "song_url": self.song_url,
+        "user": self.user_id,
+        "songs":[song.to_dict() for song in self.songs]
+      }
