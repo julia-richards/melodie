fbbe569d59e7a92175f73a41cee7368449228892
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-12-31 14:32:08 -0600

    protected route issue ghost

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,35 +1,11 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom'
 
 const ProtectedRoute = props => {
-
-  if (!props.authenticated) {
-    return <Redirect to="/login"/>
-  }
-
   return (
-    <Route {...props}/>
-  );
+    <Route {...props}>
+      {(props.authenticated)? props.children  : <Redirect to="/login" />}
+    </Route>
+  )
 };
-
 export default ProtectedRoute;
-
-
-
-
-// import React from 'react';
-// import { Route, Redirect } from 'react-router-dom';
-// ​
-// const ProtectedRoute = props => {
-//   return (
-//     <Route {...props}>
-//       {(props.authenticated)? props.children  : <Redirect to="/login" />}
-//     </Route>
-//   )
-// };
-// ​
-// ​
-// export default ProtectedRoute;
-
-// 1
-
