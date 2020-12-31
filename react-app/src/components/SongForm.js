ef03ee0c45c2f31b2ed25030ae959d2928847388
julia-richards <julia.m.richards@gmail.com>
2020-12-30 18:07:16 -0800

    Format w/ prettier

--- react-app/src/components/SongForm.js
+++ react-app/src/components/SongForm.js
@@ -13,8 +13,8 @@ const SongForm = () => {
 
   const [redirect, setRedirect] = useState(null);
 
-  const [isImageUploading, setIsImageUploading] = useState(false)
-  const [isSongUploading, setIsSongUploading] = useState(false)
+  const [isImageUploading, setIsImageUploading] = useState(false);
+  const [isSongUploading, setIsSongUploading] = useState(false);
 
   const uploadNewSong = async (e) => {
     e.preventDefault();
@@ -26,7 +26,7 @@ const SongForm = () => {
       songUrl
     );
     console.log("added song ID", song.id);
-    setRedirect(`/profile`) // TODO: update me to song?
+    setRedirect(`/profile`); // TODO: update me to song?
   };
 
   const updateTitle = (e) => {
@@ -37,93 +37,116 @@ const SongForm = () => {
     setDescription(e.target.value);
   };
 
-  const onImageDrop = useCallback(async (acceptedFiles) => {
-    setIsImageUploading(true)
-    const imageFile = acceptedFiles[0]; // only 1 accepted
-		const res = await uploadFile(imageFile);
-    setImageUrl(res.url)
-    setIsImageUploading(false)
-  }, [setImageUrl, setIsImageUploading]);
-  const { getRootProps: getImageRootProps, getInputProps: getImageInputProps, isDragActive: isImageDragActive } = useDropzone({ onDrop: onImageDrop });
+  const onImageDrop = useCallback(
+    async (acceptedFiles) => {
+      setIsImageUploading(true);
+      const imageFile = acceptedFiles[0]; // only 1 accepted
+      const res = await uploadFile(imageFile);
+      setImageUrl(res.url);
+      setIsImageUploading(false);
+    },
+    [setImageUrl, setIsImageUploading]
+  );
+  const {
+    getRootProps: getImageRootProps,
+    getInputProps: getImageInputProps,
+    isDragActive: isImageDragActive,
+  } = useDropzone({ onDrop: onImageDrop });
 
-
-
-  const onSongDrop = useCallback(async (acceptedFiles) => {
-    setIsSongUploading(true)
-    const songFile = acceptedFiles[0]; // only 1 accepted
-		const res = await uploadFile(songFile);
-    setSongUrl(res.url)
-    setIsSongUploading(false)
-  }, [setSongUrl, setIsSongUploading]);
-  const { getRootProps: getSongRootProps, getInputProps: getSongInputProps, isDragActive: isSongDragActive } = useDropzone({ onDrop: onSongDrop });
-
+  const onSongDrop = useCallback(
+    async (acceptedFiles) => {
+      setIsSongUploading(true);
+      const songFile = acceptedFiles[0]; // only 1 accepted
+      const res = await uploadFile(songFile);
+      setSongUrl(res.url);
+      setIsSongUploading(false);
+    },
+    [setSongUrl, setIsSongUploading]
+  );
+  const {
+    getRootProps: getSongRootProps,
+    getInputProps: getSongInputProps,
+    isDragActive: isSongDragActive,
+  } = useDropzone({ onDrop: onSongDrop });
 
   if (redirect) {
-    return <Redirect to={redirect} />
+    return <Redirect to={redirect} />;
   }
 
   return (
-		<div className="song_form-outer">
-			<div className="song-form_container">
-				<form className="song-form" onSubmit={uploadNewSong}>
-					<div>
-						<label>Song Title</label>
-						<input
-							type="text"
-							name="title"
-							onChange={updateTitle}
-							value={title}
-						></input>
-					</div>
-					<div>
-						<label>Description</label>
-						<textarea
-							type="text"
-							name="description"
-							onChange={updateDescription}
-							value={description}
-						></textarea>
-					</div>
-					<div>
-						<label>Song Image</label>
+    <div className="song_form-outer">
+      <div className="song-form_container">
+        <form className="song-form" onSubmit={uploadNewSong}>
+          <div>
+            <label>Song Title</label>
+            <input
+              type="text"
+              name="title"
+              onChange={updateTitle}
+              value={title}
+            ></input>
+          </div>
+          <div>
+            <label>Description</label>
+            <textarea
+              type="text"
+              name="description"
+              onChange={updateDescription}
+              value={description}
+            ></textarea>
+          </div>
+          <div>
+            <label>Song Image</label>
             <div {...getImageRootProps()}>
               <input {...getImageInputProps()} />
-              {
-                !!imageUrl ? <><img src={imageUrl} style={{maxWidth: 60, height: "auto"}}/> <button onClick={()=>setImageUrl(null)}>Remove Image</button> </>:
-                isImageUploading ? <p>Uploading...</p>:
-                isImageDragActive ?
-                  <p>Drop the file here ...</p> :
-                  <p>Drag 'n' drop image file here, or click to select files</p>
-              }
-
+              {!!imageUrl ? (
+                <>
+                  <img
+                    src={imageUrl}
+                    style={{ maxWidth: 60, height: "auto" }}
+                  />{" "}
+                  <button onClick={() => setImageUrl(null)}>
+                    Remove Image
+                  </button>{" "}
+                </>
+              ) : isImageUploading ? (
+                <p>Uploading...</p>
+              ) : isImageDragActive ? (
+                <p>Drop the file here ...</p>
+              ) : (
+                <p>Drag 'n' drop image file here, or click to select files</p>
+              )}
             </div>
-					</div>
-					<div>
-						<label>Song Audio</label>
+          </div>
+          <div>
+            <label>Song Audio</label>
 
             <div {...getSongRootProps()}>
               <input {...getSongInputProps()} />
-              {
-                !!songUrl ? <><p>TODO:Add song preview here</p> <button onClick={()=>setSongUrl(null)}>Remove Song</button> </>:
-                isSongUploading ? <p>Uploading...</p>:
-                isSongDragActive ?
-                  <p>Drop the file ...</p> :
-                  <p>Drag 'n' drop song file here, or click to select files</p>
-              }
+              {!!songUrl ? (
+                <>
+                  <p>TODO:Add song preview here</p>{" "}
+                  <button onClick={() => setSongUrl(null)}>Remove Song</button>{" "}
+                </>
+              ) : isSongUploading ? (
+                <p>Uploading...</p>
+              ) : isSongDragActive ? (
+                <p>Drop the file ...</p>
+              ) : (
+                <p>Drag 'n' drop song file here, or click to select files</p>
+              )}
+            </div>
+          </div>
 
-
-						</div>
-					</div>
-
-					<div>
-						<button className="upload_song-button" type="submit">
-							Upload
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
+          <div>
+            <button className="upload_song-button" type="submit">
+              Upload
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
-};;
+};
 
 export default SongForm;
