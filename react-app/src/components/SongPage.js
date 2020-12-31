955f045214c191e4e5e96da7634e0e9ee6edb066
salina kuo <salina.kuo97@gmail.com>
2020-12-31 11:52:11 -0500

    adding user data

--- react-app/src/components/SongPage.js
+++ react-app/src/components/SongPage.js
@@ -5,24 +5,9 @@ import { getUserById, getSongById } from "../services/song";
 
 const SongPage = () => {
     const { songId } = useParams();
-    // const [songs, setSongs] = useState([]);
     const [playingSong, setPlayingSong] = useState(null);
     const [songUser, setSongUser] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-
-	// useEffect(() => {
-	// 	(async () => {
-	// 		const response = await fetch("/api/songs/");
-	// 		const songs = await response.json();
-	// 		setSongs(songs.songs);
-    //     })()
-    // }, []);
-
-    // useEffect(() => {
-    //     let filteredSong = songs[songId - 1]
-    //     setPlayingSong(filteredSong);
-    //     setIsLoading(false)
-    // }, [songs, songId])
 
     useEffect(() => {
         (async () => {
@@ -33,18 +18,18 @@ const SongPage = () => {
             setSongUser(userRes);
         })()
         .then(setIsLoading(false));
-    }, [])
+    }, [songId])
 
-    if (isLoading || !playingSong) return null;
+    if (isLoading || !playingSong || !songUser) return null;
 
-    if (playingSong) {
-        console.log(playingSong)
+    if (songUser) {
+        console.log(songUser)
     }
 
     return (
         <div>
             <h2>{playingSong.title}</h2>
-            {/* <h3>{songUser.username}</h3> */}
+            <h3>{songUser.username}</h3>
             <img src={playingSong.image_url}></img>
             <SongPlayer playingSong={playingSong}/>
         </div>
