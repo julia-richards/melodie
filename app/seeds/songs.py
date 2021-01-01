72ef8083f5c7d6e6d7e806a688a8ee551c33e03c
salina kuo <salina.kuo97@gmail.com>
2021-01-01 15:51:26 -0500

    adding songs and users

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -2,7 +2,13 @@ from app.models import db, Song
 
 def seed_songs():
 
-    sample = Song(title='New Years Eve', description='', image_url='https://melodieapp.s3.amazonaws.com/Untitled_Artwork.jpg', song_url='https://melodieapp.s3.amazonaws.com/NYE+mixed.wav', user_id=1)
+    nye = Song(title='What are you doing, New Year\'s Eve?', description='vibraphone, vocals - Salina\nhorns - Pat\nguitar/bass/drums/audio - Robbie', image_url='https://melodieapp.s3.amazonaws.com/salinavibe.jpeg', song_url='https://melodieapp.s3.amazonaws.com/NYE+mixed.wav', user_id=2)
+
+    prettygirl = Song(title='pretty girl', description='pretty girl by clairo\nvibraphone/drums/vocals/mixing - Salina', image_url='https://melodieapp.s3.amazonaws.com/salinavibe.jpeg', song_url='https://melodieapp.s3.amazonaws.com/pretty+girl.wav', user_id=2)
+
+    ghostsanta = Song(title='Ghost Santa', description='A holiday-themed parody of "Ghost Riders in the Sky" that follows the horrific tale of Ghost Santa.\nEric - acoustic guitar, banjo, vocals, lyrics\nRobbie - electric guitar, bass, piano, mandolin, drums, jingle bells, cigar box made by Mark, cheese\nCover art cross-stitched by Christina', image_url='https://melodieapp.s3.amazonaws.com/frootcake+album+art.JPG', user_id=3)
+
+    sweetsilverbells = Song(title='Sweet Silver Bells', description='A semi-experimental hoilday piece. Can you figure out what the crackly voice is saying to you?\nRobbie - acoustic guitar, vocals, drums, synth\nCover art cross-stitched by Christina', image_url='https://melodieapp.s3.amazonaws.com/frootcake+album+art.JPG', user_id=3)
 
     db.session.add(sample)
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -7,12 +7,15 @@ def seed_users():
     demo = User(username='Demo', email='demo@aa.io',
                 password='password', about="testing this stuff out", image_url='tesing this url')
 
+    stjohnswort = User(username='St. John\'s Wort', email='stjohnswort@aa.io', password='password', about="I like pretty sounds.\nPhiladelphia, PA", image_url='tesing this url')
+
+    mudfroot = User(username='Mudfroot', email='mudfroot@aa.io', password='password', about="bluegrass from space", image_url='https://melodieapp.s3.amazonaws.com/mudfroot.JPG')
 
     Prince = User(username='Prince', email='prince@yahoo.com',
                 password='password123', about='legendary', image_url='princephoto change to url')
 
 
-    
+
 
     db.session.add(demo)
     db.session.add(Prince)
