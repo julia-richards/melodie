8ea5be78360ddd0f76e1dbe35e71e0954d861f13
salina kuo <salina.kuo97@gmail.com>
2021-01-01 22:48:20 -0500

    styling song form

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,38 +6,40 @@ import "../styles/NavBar.css"
 const NavBar = ({ setAuthenticated }) => {
 	return (
 		<nav className="navbar">
-			<ul className="links">
+			<ul className="linkList">
 				<li>
-					<NavLink to="/" exact={true} activeClassName="active">
+					<NavLink to="/" exact={true} className="links" activeClassName="active">
 						Home
 					</NavLink>
 				</li>
 				<li>
-					<NavLink to="/login" exact={true} activeClassName="active">
-						Login
+					<NavLink to="/login" exact={true} className="links" activeClassName="active">
+						Log In
 					</NavLink>
 				</li>
 				<li>
 					<NavLink
 						to="/sign-up"
 						exact={true}
+						className="links"
 						activeClassName="active"
 					>
 						Sign Up
 					</NavLink>
 				</li>
-				<li>
-					<NavLink to="/users" exact={true} activeClassName="active">
+				{/* <li>
+					<NavLink to="/users" exact={true} className="links" activeClassName="active">
 						Users
 					</NavLink>
-				</li>
+				</li> */}
 				<li>
 					<NavLink
 						to="/songs/upload"
 						exact={true}
+						className="links"
 						activeClassName="active"
 					>
-						Upload New Song
+						Upload
 					</NavLink>
 				</li>
 				<li>
