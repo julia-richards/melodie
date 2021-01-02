28f39fd99a2f226f0f149617d06eec3e50b74190
Rashmi <monajain99@hotmail.com>
2021-01-02 15:10:32 -0500

    editSong

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -3,21 +3,13 @@ from flask_login import login_required, current_user
 from app.models import db, Song, User
 from app.s3 import upload_file
 from app.forms.song_form import SongForm
-from app.forms.edit_song_form import EditSongForm
-
 
 song_routes = Blueprint('songs', __name__)
-
-
-
 
 @song_routes.route('/')
 def songs():
     songs = Song.query.all()
     return {"songs": [song.to_dict() for song in songs]}
-
-
-
 
 @song_routes.route('/', methods=["POST"])
 @login_required
@@ -64,7 +56,33 @@ def update_song(id):
         return {'errors': form.errors}, 422
     return {'errors': 'Only the artist can delete this song'}, 401
 
-
+@song_routes.route('/<int:id>/edit', methods=['PATCH'])
+# @login_required
+def edit_song(id):
+    form = SongForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if request.files:
+            file = request.files.get('file')
+            url = upload_file(file)
+            return {'url': url}
+        song = Song.query.get(id)
+        # if song.user_id == current_user.id:
+        data = request.json
+        print(data)
+        if form.data['title']:
+            song.title = data['title']
+        if form.data['description']:
+            song.description = data['description']
+        if form.data['image_url']:
+            song.about = data['image_url']
+        if form.data['song_url']:
+            song.image_url = data['song_url']
+        db.session.commit()
+        return song.to_dict()
+        # return {'errors': 'Only the artist can edit this song'}, 401
+    return {'errors': form.errors}, 422
+        
 @song_routes.route("/upload", methods=['POST'])
 @login_required
 def upload():
@@ -73,7 +91,6 @@ def upload():
         url = upload_file(file)
 
         return {'url': url}
-
 
 @song_routes.route('/<int:id>/likes', methods=["POST"])
 @login_required
