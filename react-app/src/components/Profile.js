d7d329a0fd9407c375399b708e9087c4d1a41b68
Rashmi <monajain99@hotmail.com>
2021-01-03 12:14:34 -0500

    Edit

--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -19,8 +19,8 @@ const Profile = () => {
       const res1 = await getUserLikedSongs(profileId);
       const res2 = await getAllSongs(profileId);
 
-      setProfile(res1);
-      setUsersSongs(res)
+      setProfile(res);
+      setUsersSongs(res1)
       setAllSongs(res2);
     })();
   }, []);
@@ -48,6 +48,7 @@ const Profile = () => {
             <h3>All Songs</h3>
             {/* {allSongs && <h3>Song List</h3>}
             <div> */}
+            
             {allSongs &&
               allSongs.songs.map((song, id) => (
                 <Songs song={song} key={song.id} />
