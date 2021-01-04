f87e5b52a938a81f256ac8ecfcdc558f6b693b4b
julia-richards <julia.m.richards@gmail.com>
2021-01-03 19:40:43 -0700

    fixes julia's logout bug

--- react-app/src/components/auth/ProfileButton.js
+++ react-app/src/components/auth/ProfileButton.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { logout } from "../../services/auth";
 
-const ProfileButton = ({ user, onLogoutClick }) => {
+const ProfileButton = ({ user, setAuthenticated }) => {
 	const [showMenu, setShowMenu] = useState(false);
 
 	const openMenu = () => {
 		if (showMenu) return;
 		setShowMenu(true);
 	};
+
+	const onLogoutClick = async () => {
+		await logout().then(() => setAuthenticated(false));
+	};;
 
 	useEffect(() => {
 		if (!showMenu) return;
