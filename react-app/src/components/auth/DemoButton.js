b5783f60e2b8c7df97cf1dba9c806c225730341a
julia-richards <julia.m.richards@gmail.com>
2021-01-03 20:54:55 -0700

    moves towards unified styles

--- react-app/src/components/auth/DemoButton.js
+++ react-app/src/components/auth/DemoButton.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { demo } from "../../services/auth";
 
-const DemoButton = ({ setAuthenticated, authenticated }) => {
-  const demobutton = async (e) => {
-    await demo();
-    setAuthenticated(true);
-    window.location.reload(false);
+const DemoButton = ({ setAuthenticated, authenticated, isLink = false }) => {
+	const handleLoginClick = async (e) => {
+		await demo();
+		setAuthenticated(true);
+		window.location.reload(false);
   };
 
-  return authenticated ? (
-    ""
-  ) : (
-    <button className="navBtn" onClick={demobutton}>
+  if (authenticated) return null;
+
+  if (isLink) {
+    return (
+      <a href="#" style={{ cursor: "pointer" }} onClick={handleLoginClick}>
+        Login as Demo User
+      </a>
+    )
+  }
+
+  return (
+    <button className="navBtn" onClick={handleLoginClick}>
       Login as a Demo User
     </button>
-  );
+  )
 };
 
 export default DemoButton;
-
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import { login } from "../../services/auth";
 import Input from './Input.js'
 import styled from 'styled-components'
 import cropped from '../../assets/cropped.png'
-
+import DemoButton from "./DemoButton";
+import '../../styles/LoginForm.css'
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const [errors, setErrors] = useState([]);
@@ -34,11 +35,11 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   }
 
   return (
-    <div className="song_form-outer">
-      <div className="song-form_container">
-        <Form className="song-form" onSubmit={onLogin}>
+    <div className="login-form__outer">
+      <div className="login-form__container">
+        <Form className="login-form" onSubmit={onLogin}>
       <LogoWrapper>
-        <img src={cropped} alt='' />
+        <img src={cropped} alt='logo' />
       </LogoWrapper>
           <div>
             {errors.map((error) => (
@@ -61,25 +62,11 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
             />
             <button type="submit">Login</button>
         </Form>
+        <div className="login-form__redirect-text">Don't have an account?<br /> <Link to='/sign-up'>Sign Up</Link> or <DemoButton setAuthenticated={setAuthenticated} isLink /></div>
       </div>
     </div>
   );
 };
-
-
-
-const Welcome = styled.div`
-h1{
-
-
-font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-font-style: normal;
-font-weight: lighter;
-font-size: 50px;
-line-height: 12px;
-color: #ffffff;
-}`
-
 
 const Form = styled.form`
   width: 100%;
@@ -114,46 +101,8 @@ const Form = styled.form`
 const LogoWrapper = styled.div`
   img {
     height: 6rem;
-  }
-  h3 {
-    color: #FFF0F0;
-    text-align: center;
-    font-size: 22px;
-  }
-  span {
-    color: #5dc399;
-    font-weight: 300;
-    font-size: 18px;
+    margin: 2rem auto 1rem;
   }
 `;
-
-
-const Container = styled.div`
-  min-width: 400px;
-  backdrop-filter: blur(35px);
-  /* background-color: rgba(255, 255, 255, 0.8); */
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 0 2rem;
-  @media (max-width: 900px) {
-    width: 100vw;
-    position: absolute;
-    padding: 0;
-  }
-  h4 {
-    color: #808080;
-    font-weight: bold;
-    font-size: 13px;
-    margin-top: 2rem;
-    span {
-      color: #ff8d8d;
-      cursor: pointer;
-    }
-  }
-`;
-
 
 export default LoginForm;
