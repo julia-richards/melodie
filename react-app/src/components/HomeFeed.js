1ab6715ed643cb273fecb68189b4d1e02a01f7fa
julia-richards <julia.m.richards@gmail.com>
2021-02-01 18:49:52 -0700

    renders like button on song preview

--- react-app/src/components/HomeFeed.js
+++ react-app/src/components/HomeFeed.js
@@ -1,35 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Songs from "./Songs";
 
-
 const HomeFeed = () => {
-    const [songList, setSongList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [songList, setSongList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setIsLoading(true);
-		(async () => {
-			const response = await fetch("/api/songs/");
-			const songs = await response.json();
-			setSongList(songs.songs);
-		})();
-        setIsLoading(false);
-    }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      const response = await fetch("/api/songs/");
+      const songs = await response.json();
+      setSongList(songs.songs);
+    })();
+    setIsLoading(false);
+  }, []);
 
-    if (!songList || isLoading) return null;
+  if (!songList || isLoading) return null;
 
-
-
-    return (
-        <div className="wholePageContainer">
-            <div className="bodyContainer">
-                <div className="feedContainer">
-                    <h1 className="heading">Featured</h1>
-                    <Songs  songList={songList}/>
-                </div>
-            </div>
+  return (
+    <div className="wholePageContainer">
+      <div className="bodyContainer">
+        <div className="feedContainer">
+          <h1 className="heading">Featured</h1>
+          <Songs songList={songList} />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default HomeFeed;
